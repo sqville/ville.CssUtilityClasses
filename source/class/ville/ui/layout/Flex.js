4f43be4058b124b48055471777479307606f0d64
Chris Eskew <sqville@gmail.com>
2026-02-10 10:27:47 -0600

    Updated Flex.js to add css class via code
    Updated Flex.js to add css class via code. The mantine css file Flex.css only has one class and one rule. Seems like too much overhead (download external css file) for one line of css

--- source/class/ville/ui/layout/Flex.js
+++ source/class/ville/ui/layout/Flex.js
@@ -1,6 +1,7 @@
 /**
  * Horizontal flex layout
- * @external(mantine/core/styles/Flex.css)
+ * 2/10/2026 - removed external call out to "mantine/core/styles/Flex.css"
+ * and replaced with addRule call. Too much overhead for one css class with one rule
  */
 qx.Class.define("ville.ui.layout.Flex", {
     
@@ -11,6 +12,12 @@ qx.Class.define("ville.ui.layout.Flex", {
     construct() {
 
         super();
+
+        var sheet = qx.ui.style.Stylesheet.getInstance();
+        
+        if (!sheet.hasRule("m_8bffd616")) {
+            sheet.addRule("m_8bffd616", "display: flex;");
+        }
 
         this.setCssUtilityClass("m_8bffd616 mantine-Flex-root");
 
